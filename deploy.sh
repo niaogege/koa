7e0c3c5b0fbea13a28a233c8c2f946adf567c78a
chendapeng <pengpeng.chen@ximalaya.com>
2022-12-02 18:23:56 +0800

    feat: change3

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,17 @@ set -e
 # cd dist
 pm2 start ./index.js
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='cpp1 from github actions auto deploy commit'
-  githubUrl=git@github.com:niaogege/koa.git
-else
-  msg='cpp2 from github actions auto deploy commit'
-  githubUrl=https://niaogege:${GITHUB_TOKEN}@github.com/niaogege/koa.git
-  git config --local user.name "niaogege"
-  git config --local user.email "291003932@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-# git checkout -B gh-pages
-git push -f ${githubUrl} main:gh-pages # 推送到github gh-pages分支
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='cpp1 from github actions auto deploy commit'
+#   githubUrl=git@github.com:niaogege/koa.git
+# else
+#   msg='cpp2 from github actions auto deploy commit'
+#   githubUrl=https://niaogege:${GITHUB_TOKEN}@github.com/niaogege/koa.git
+#   git config --local user.name "niaogege"
+#   git config --local user.email "291003932@qq.com"
+# fi
+# git init
+# git add -A
+# git commit -m "${msg}"
+# # git checkout -B gh-pages
+# git push -f ${githubUrl} main:gh-pages # 推送到github gh-pages分支
